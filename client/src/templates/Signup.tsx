c8b2cb4e1808562ac012c925cdc1953fc9add1f7
Rebeca Araujo <104568778+ReisLimaaaa@users.noreply.github.com>
2023-09-05 12:59:20 +0000

    ...

--- client/src/templates/Signup.tsx
+++ client/src/templates/Signup.tsx
@@ -1,3 +1,36 @@
+import { styled } from "styled-components";
+import bgimg from "../assets/flor.jpg"
+import FormSignup from "../components/FormSignup";
+
+const Container = styled.div`
+    background-image: url(${bgimg});
+    background-size: cover;
+    height: 100vh;
+    display: grid;
+    place-content: center;
+    position: relative;
+    overflow: hidden;
+
+    &::after {
+        content: " ";
+        background-color: #243238;
+        width: 90%;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        z-index: 1;
+        transform: skew(15deg) translateX(50%);
+        box-shadow: -2px -2px 5px #000000;
+    }
+
+    > * {
+        position: relative;
+        z-index: 9;
+    }
+`
+
 export default () => <>
-    FORMUlário de Cadastro
+    <Container>
+        <FormSignup />
+    </Container>
 </>
